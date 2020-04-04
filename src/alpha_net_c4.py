d99aa850effd4cf4c5436c3c07383cab7071c5e6
ZehaoJin <zjin16@illinois.edu>
2020-04-04 18:20:51 -0500

    debug+

--- src/alpha_net_c4.py
+++ src/alpha_net_c4.py
@@ -13,22 +13,22 @@ class board_data(Dataset):
     def __init__(self, dataset): # dataset = np.array of (s, p, v)
         self.X = dataset[:,0]
         self.y_p, self.y_v = dataset[:,1], dataset[:,2]
-    
+
     def __len__(self):
         return len(self.X)
-    
+
     def __getitem__(self,idx):
         return np.int64(self.X[idx].transpose(2,0,1)), self.y_p[idx], self.y_v[idx]
 
 class ConvBlock(nn.Module):
     def __init__(self):
         super(ConvBlock, self).__init__()
-        self.action_size = 7
+        self.action_size = 9
         self.conv1 = nn.Conv2d(3, 128, 3, stride=1, padding=1)
         self.bn1 = nn.BatchNorm2d(128)
 
     def forward(self, s):
-        s = s.view(-1, 3, 6, 7)  # batch_size x channels x board_x x board_y
+        s = s.view(-1, 3, 3, 3)  # batch_size x channels x board_x x board_y
         s = F.relu(self.bn1(self.conv1(s)))
         return s
 
@@ -51,32 +51,32 @@ class ResBlock(nn.Module):
         out += residual
         out = F.relu(out)
         return out
-    
+
 class OutBlock(nn.Module):
     def __init__(self):
         super(OutBlock, self).__init__()
         self.conv = nn.Conv2d(128, 3, kernel_size=1) # value head
         self.bn = nn.BatchNorm2d(3)
-        self.fc1 = nn.Linear(3*6*7, 32)
+        self.fc1 = nn.Linear(3*3*3, 32)
         self.fc2 = nn.Linear(32, 1)
-        
+
         self.conv1 = nn.Conv2d(128, 32, kernel_size=1) # policy head
         self.bn1 = nn.BatchNorm2d(32)
         self.logsoftmax = nn.LogSoftmax(dim=1)
-        self.fc = nn.Linear(6*7*32, 7)
-    
+        self.fc = nn.Linear(3*3*32, 7)
+
     def forward(self,s):
         v = F.relu(self.bn(self.conv(s))) # value head
-        v = v.view(-1, 3*6*7)  # batch_size X channel X height X width
+        v = v.view(-1, 3*3*3)  # batch_size X channel X height X width
         v = F.relu(self.fc1(v))
         v = torch.tanh(self.fc2(v))
-        
+
         p = F.relu(self.bn1(self.conv1(s))) # policy head
-        p = p.view(-1, 6*7*32)
+        p = p.view(-1, 3*3*32)
         p = self.fc(p)
         p = self.logsoftmax(p).exp()
         return p, v
-    
+
 class ConnectNet(nn.Module):
     def __init__(self):
         super(ConnectNet, self).__init__()
@@ -84,14 +84,14 @@ class ConnectNet(nn.Module):
         for block in range(19):
             setattr(self, "res_%i" % block,ResBlock())
         self.outblock = OutBlock()
-    
+
     def forward(self,s):
         s = self.conv(s)
         for block in range(19):
             s = getattr(self, "res_%i" % block)(s)
         s = self.outblock(s)
         return s
-        
+
 
 class AlphaLoss(torch.nn.Module):
     def __init__(self):
@@ -99,7 +99,7 @@ class AlphaLoss(torch.nn.Module):
 
     def forward(self, y_value, value, y_policy, policy):
         value_error = (value - y_value) ** 2
-        policy_error = torch.sum((-policy* 
+        policy_error = torch.sum((-policy*
                                 (1e-8 + y_policy.float()).float().log()), 1)
         total_error = (value_error.view(-1).float() + policy_error).mean()
         return total_error
